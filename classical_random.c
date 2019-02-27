9c0dc3fd0d86c2319271edbf91a6f3d111dfb219
mle <mle@esi-group.com>
2019-02-27 10:06:33 +0100

    added classical random in C

--- classical_random.c
+++ classical_random.c
@@ -4,10 +4,34 @@
 #include <time.h>
 
 
-
 int main(int argc,const char *argv[])
 
 {
-    //TODO
-    printf("Good start!");
+    srand(0);  // seed
+
+    printf("rand int between 0 and 4:\n");
+    for (int i=0; i<20; i++)
+    {
+        printf("%d ",rand()%5);
+    }
+
+    printf("\n\nrand int :\n");
+    
+    for (int i=0; i<20; i++)
+    {
+        printf("%d ",rand());
+    }
+
+    printf("\n RAND_MAX value: %d \n:", RAND_MAX);
+
+    printf("\n\nrand between 0 and 1 :\n");
+    
+    for (int i=0; i<20; i++)
+    {
+        printf("%f ", (double)rand() / (double)RAND_MAX );
+    }
+
+    
+
+
 }
