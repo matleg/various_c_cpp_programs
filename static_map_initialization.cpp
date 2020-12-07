e1c4206a9be5b0d4713f5315b02fc701da2882aa
LEGAY <mathieu.legay@cgi.com>
2020-12-07 18:17:30 +0100

    add unordered map and set init

--- static_map_initialization.cpp
+++ static_map_initialization.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
 
 using namespace std;
-
-class Game
-{
-public:
-    static map<string, int> initEntityMap()
-    {
-        map<string, int> m;
-        m["Ball"] = 0;
-        m["Player"] = 1;
-        m["Referee"] = 2;
-        return m;
-    }
-    const static map<string, int> entityMap;
-};
-
-const map<string, int> Game::entityMap = Game::initEntityMap();
-
-Game game;
 
 int main()
 {
 
-    cout << game.entityMap.at("Ball") << endl;
+    map<string, int> ordered;
+    ordered["hello"] = 1;
+    ordered["world"] = 2;
+    for (auto &p : ordered)
+        cout << p.first << p.second << endl;
+    cout << "ordered map size:" << ordered.size() << endl
+         << endl;
+
+    unordered_map<string, int> unordered;
+    unordered["hello"] = 3;
+    for (auto &p : unordered)
+        cout << p.first << p.second << endl;
+    cout << "unordered map size:" << unordered.size() << endl
+         << endl;
+
+    set<int> _s;
+    _s.insert(543);
+    _s.insert(123);
+    if (_s.find(123) != _s.end())
+        cout << "found: 123" << endl;
+    cout << "set size:" << _s.size() << endl
+         << endl;
+
+    unordered_set<int> unordered_s;
+    unordered_s.insert(123);
+    if (unordered_s.find(123) != unordered_s.end())
+        unordered_s.erase(123);
+    cout << "unordered set size:" << unordered_s.size() << endl
+         << endl;
 }
